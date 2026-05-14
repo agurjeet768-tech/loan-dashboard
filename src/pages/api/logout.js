54736ecfe0d32376d85099cd654c09a67bcd26a9
agurjeet768-tech <agurjeet768@gmail.com>
2026-05-14 20:01:11 +0530

    Delete src/pages/api directory

--- src/pages/api/logout.js
+++
@@ -1,8 +0,0 @@
-// src/pages/api/logout.js
-export const prerender = false;
-
-export async function POST() {
-  return new Response(JSON.stringify({ success: true }), {
-    headers: { 'Content-Type': 'application/json' }
-  });
-}
